14cc9ac11c39216541cc4a25ddcaa7dc720cbf31
unknown <princariens@gmail.com>
2022-04-18 20:01:06 +0500

    gender

--- get_gender_users.py
+++ get_gender_users.py
@@ -13,13 +13,26 @@ def get_gender_users(data:dict) -> list:
     Returns:
         list: users get gender list
     """
-    genders=[{"Male":0},{"Female":0}]
+    # genders=[{"Male":0},{"Female":0}]
+    # for i in data['results']:
+    #     if i.get('gender')=='male':
+    #         genders[0]['Male']+=1
+    #     elif i.get('gender')=='female':
+    #         genders[1]['Female']+=1
+    # return genders
+    users_data = []
+    user= {}
     for i in data['results']:
+        user['first_name']=i['name']['first']
+        user['last_name']=i['name']['last']
+        user['phone_number']=i['phone']
         if i.get('gender')=='male':
-            genders[0]['Male']+=1
-        elif i.get('gender')=='female':
-            genders[1]['Female']+=1
-    return genders
+            user["Male"]=1
+        else:
+            user["Female"]=0
+        users_data.append(user)
+        user={}
+    return users_data
 
 
 f=open('./randomuser_data.json')
